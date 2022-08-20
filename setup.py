70e50616ae994965a88b708ec3930140b1ea1573
Makoto Shimizu <mak@fish.razor.jp>
2022-08-21 01:07:38 +0900

    debug

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='megaton_data',
-    version='1.0.4',
+    version='1.0.5',
     author='Makoto Shimizu',
     author_email='aa.analyst.ga@gmail.com',
     description='Python utilities for Pardot API.',
