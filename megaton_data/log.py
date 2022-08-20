d9b65ef9047da0f2c423f8511eabce1d863861b0
Makoto Shimizu <mak@fish.razor.jp>
2022-08-21 01:04:30 +0900

    debug

--- megaton_data/log.py
+++ megaton_data/log.py
@@ -2,6 +2,7 @@ import json
 from logging import getLogger, INFO, WARNING, ERROR
 import os
 
+# executed in Google Cloud Functions?
 IN_GCF = False
 if os.getenv('FUNCTION_REGION') or os.getenv('FUNCTION_TARGET'):
     IN_GCF = True
@@ -18,26 +19,43 @@ class Logger:
         if not IN_GCF:
             self.logger.setLevel(level)
 
-    def debug(self, msg: str, *args, **kwargs):
-        if IN_GCF and self.level not in (INFO, WARNING, ERROR):
-            print(json.dumps(dict(severity='DEBUG', message=msg)))
-        else:
+    def output(self, msg: str, *args, **kwargs):
+        if not IN_GCF:
             self.logger.debug(msg, *args, **kwargs)
+        else:
+            if self.level not in (INFO, WARNING, ERROR):
+                print(json.dumps(dict(severity='DEBUG', message=msg)))
+            elif self.level not in (WARNING, ERROR):
+                print(json.dumps(dict(severity='INFO', message=msg)))
+            elif self.level is not ERROR:
+                print(json.dumps(dict(severity='WARNING', message=msg)))
+            else:
+                print(json.dumps(dict(severity='ERROR', message=msg)))
+
+    def debug(self, msg: str, *args, **kwargs):
+        self.output(msg, *args, **kwargs)
+        # if IN_GCF and self.level not in (INFO, WARNING, ERROR):
+        #     print(json.dumps(dict(severity='DEBUG', message=msg)))
+        # else:
+        #     self.logger.debug(msg, *args, **kwargs)
 
     def info(self, msg: str, *args, **kwargs):
-        if IN_GCF and self.level not in (WARNING, ERROR):
-            print(json.dumps(dict(severity='INFO', message=msg)))
-        else:
-            self.logger.info(msg, *args, **kwargs)
+        self.output(msg, *args, **kwargs)
+        # if IN_GCF and self.level not in (WARNING, ERROR):
+        #     print(json.dumps(dict(severity='INFO', message=msg)))
+        # else:
+        #     self.logger.info(msg, *args, **kwargs)
 
     def warning(self, msg: str, *args, **kwargs):
-        if IN_GCF and self.level is not ERROR:
-            print(json.dumps(dict(severity='WARNING', message=msg)))
-        else:
-            self.logger.warning(msg, *args, **kwargs)
+        self.output(msg, *args, **kwargs)
+        # if IN_GCF and self.level is not ERROR:
+        #     print(json.dumps(dict(severity='WARNING', message=msg)))
+        # else:
+        #     self.logger.warning(msg, *args, **kwargs)
 
     def error(self, msg: str, *args, **kwargs):
-        if IN_GCF:
-            print(json.dumps(dict(severity='ERROR', message=msg)))
-        else:
-            self.logger.error(msg, *args, **kwargs)
+        self.output(msg, *args, **kwargs)
+        # if IN_GCF:
+        #     print(json.dumps(dict(severity='ERROR', message=msg)))
+        # else:
+        #     self.logger.error(msg, *args, **kwargs)
