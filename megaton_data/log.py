3457a94f59c6ebc3cd3bbebf4954a02fbb1fe20d
Makoto Shimizu <mak@fish.razor.jp>
2022-08-21 01:14:18 +0900

    debug

--- megaton_data/log.py
+++ megaton_data/log.py
@@ -34,28 +34,12 @@ class Logger:
 
     def debug(self, msg: str, *args, **kwargs):
         self.output(msg, *args, **kwargs)
-        # if IN_GCF and self.level not in (INFO, WARNING, ERROR):
-        #     print(json.dumps(dict(severity='DEBUG', message=msg)))
-        # else:
-        #     self.logger.debug(msg, *args, **kwargs)
 
     def info(self, msg: str, *args, **kwargs):
         self.output(msg, *args, **kwargs)
-        # if IN_GCF and self.level not in (WARNING, ERROR):
-        #     print(json.dumps(dict(severity='INFO', message=msg)))
-        # else:
-        #     self.logger.info(msg, *args, **kwargs)
 
     def warning(self, msg: str, *args, **kwargs):
         self.output(msg, *args, **kwargs)
-        # if IN_GCF and self.level is not ERROR:
-        #     print(json.dumps(dict(severity='WARNING', message=msg)))
-        # else:
-        #     self.logger.warning(msg, *args, **kwargs)
 
     def error(self, msg: str, *args, **kwargs):
         self.output(msg, *args, **kwargs)
-        # if IN_GCF:
-        #     print(json.dumps(dict(severity='ERROR', message=msg)))
-        # else:
-        #     self.logger.error(msg, *args, **kwargs)
