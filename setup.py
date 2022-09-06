083c7640b618330f12263d98065c358b5936eb86
Makoto Shimizu <mak@fish.razor.jp>
2022-09-06 23:35:52 +0900

    add bigquery and ftp support

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as f:
 
 setup(
     name='megaton_data',
-    version='1.0.5',
+    version='1.0.6',
     author='Makoto Shimizu',
     author_email='aa.analyst.ga@gmail.com',
-    description='Python utilities for Pardot API.',
+    description='Python utilities for GCP and Pardot APIs.',
     long_description=readme,
     long_description_content_type='ext/markdown',
     packages=find_packages(),
