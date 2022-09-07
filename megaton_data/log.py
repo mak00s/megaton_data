f2a3f23bd6d0564c61cf102c8bb6f3d73de45706
Makoto Shimizu <mak@fish.razor.jp>
2022-09-08 00:12:19 +0900

    bug fix

--- megaton_data/log.py
+++ megaton_data/log.py
@@ -11,13 +11,13 @@ if os.getenv('FUNCTION_REGION') or os.getenv('FUNCTION_TARGET'):
 class Logger:
     def __init__(self, logger_name: str):
         self.level = WARNING
-        if not IN_GCF:
-            self.logger = getLogger(logger_name)
+        # if not IN_GCF:
+        self.logger = getLogger(logger_name)
 
     def setLevel(self, level):
         self.level = level
-        if not IN_GCF:
-            self.logger.setLevel(level)
+        # if not IN_GCF:
+        self.logger.setLevel(level)
 
     def output(self, msg: str, *args, **kwargs):
         if not IN_GCF:
