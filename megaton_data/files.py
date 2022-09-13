bda41493b5a907c44f6aa60bc9e5e5c29b99936a
Makoto Shimizu <mak@fish.razor.jp>
2022-09-13 17:11:46 +0900

    bug fix add save_df_to_file

--- megaton_data/files.py
+++ megaton_data/files.py
@@ -11,6 +11,16 @@ def cd(destination_dir: str = None):
     if not destination_dir:
         destination_dir = os.getenv('TMP_DIR', '/tmp')
 
-    logging.debug(f"Changing current directory to {destination_dir}")
+    LOGGER.debug(f"Changing current directory to {destination_dir}")
     os.makedirs(destination_dir, exist_ok=True)
     os.chdir(destination_dir)
+
+
+def save_df_to_file(df, file_path: str):
+    # Save df to a file
+    df.to_csv(file_path,
+              header=False,
+              mode='a',
+              index=False,
+              sep='\t')
+    LOGGER.info(f"Data saved to {file_path}.")
