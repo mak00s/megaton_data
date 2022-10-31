9deff144a0a36571531b5b7bbbb213a046c13a63
Makoto Shimizu <mak@fish.razor.jp>
2022-10-31 18:27:16 +0900

    refactoring

--- megaton_data/sm.py
+++ megaton_data/sm.py
@@ -1,38 +1,42 @@
-"""Common helper FTP functions"""
+import io
 import logging
 import os
 
 from google.cloud import secretmanager
+from paramiko import RSAKey
 
-PROJECT_ID = os.getenv("GCP_PROJECT")
 LOGGER = logging.getLogger(__name__)
 
-# Reuse GCP Clients across function invocations using globbals
-# https://cloud.google.com/functions/docs/bestpractices/tips#use_global_variables_to_reuse_objects_in_future_invocations
-SM_CLIENT = None
 
+class Secret:
+    def __init__(self, project_id=None):
+        if project_id:
+            self.project_id = project_id
+        else:
+            self.project_id = os.getenv("GCP_PROJECT")
 
-def lazy_client() -> secretmanager.SecretManagerServiceClient:
-    """
-    Return a BigQuery Client that may be shared between cloud function
-    invocations.
-    """
-    global SM_CLIENT
-    if not SM_CLIENT:
-        LOGGER.debug("Creating Storage Client")
-        SM_CLIENT = secretmanager.SecretManagerServiceClient()
-    return SM_CLIENT
+    def text(self, secret_id: str, version_id='latest'):
+        """
+        Access the payload for the given secret version if one exists. The version
+        can be a version number as a string (e.g. "5") or an alias (e.g. "latest").
+        """
 
+        # Create the Secret Manager client
+        client = secretmanager.SecretManagerServiceClient()
 
-def get_secret_text(project_id: str, secret_num: str):
-    """
-    Get information about the given secret
-    """
-    client = lazy_client()
-    # Build the resource name of the secret version
-    name = f"projects/{project_id}/secrets/{secret_num}/versions/latest"
+        # Build the resource name of the secret version
+        name = client.secret_version_path(self.project_id, secret_id, version_id)
 
-    # Get the secret
-    response = client.access_secret_version(request={"name": name})
+        # Get the secret version
+        response = client.access_secret_version(name=name)
 
-    return response.payload.data.decode('UTF-8')
+        # Return the decoded payload.
+        return response.payload.data.decode("UTF-8")
+
+    def private_key(self, secret_id: str):
+        if secret_id:
+            sec = self.text(secret_id).rstrip('\n')
+            LOGGER.debug(f"key found: {sec[:25]}")
+            p_key = io.StringIO(sec)
+            return RSAKey.from_private_key(p_key)
+        logging.warn(f"key NOT found for {secret_id}")
